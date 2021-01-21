c6539c139c338bbe9e184de6e15ddc1e595ae79a
Bhuvaneswari G <67910524+Bhuvaneswari-G@users.noreply.github.com>
2021-01-21 23:24:03 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="shortcut icon" href="images/cm.png">
+   
+	 <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("https://img.freepik.com/free-photo/fintech-icon-abstract-financial-technology-background_34663-91.jpg?size=626&ext=jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+	<div id="header">
+       <br>
+       <h1 style=" font-family:Agency FB; font-size: 70px; color:#FF4500;text-shadow: 2px 2px black;">The Sparks Foundation</h1>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#ee10ee;text-shadow: 2px 2px black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Basic Banking System </h2>
+       
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="primary_btn" id="button "type="button" href="getdetail.php">View users</button>
+                </a>
+               
+                </tr>
+                <tr>        
+               <br> <br> <br>
+			   <a href="transfer.php">
+			   <button class="primary_btn" id="button" type="button">Credit Transfer</button>
+               </a>
+               
+               </tr>
+			   
+			    <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="primary_btn" id="button" type="button">All Transaction</button>
+               </a>
+            </table>
+    </div>
+	<h4 style="font-family:Arial FB; font-size: 30px; color:red;text-shadow: 2px 2px orange;">Presented by,</h4>
+    <h4 style="font-family:Arial FB; font-size: 30px; color:#9ACD32;text-shadow: 2px 2px orange;">Bhuvaneswari G</h4>           
+</body>
+</html>
